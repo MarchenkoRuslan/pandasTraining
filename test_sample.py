428c86dd0243e7c98620512ec9085a9f5e47727a
MarchenkoRuslan <wolkoff07@gmail.com>
2021-12-07 18:50:35 +0200

    test fixed

--- test_sample.py
+++ test_sample.py
@@ -2,9 +2,16 @@ import pytest
 import pandas as pd
 import os
 import main
+from pandas.testing import assert_frame_equal
+from pandas.api.types import CategoricalDtype
 
 files = main.files
 pull = []
+data_types = {'AccountNumber': 'object',
+              'SecurityCode': CategoricalDtype(categories=['EF', 'MF', 'OT'], ordered=False),
+              'Price': 'float64',
+              'TransactionDate': 'object',
+              '_merge': CategoricalDtype(categories=['left_only', 'right_only', 'both'], ordered=False)}
 
 for file in os.listdir(files):
     if file.endswith(".csv"):
@@ -15,9 +22,10 @@ for file in os.listdir(files):
 def test_sample(dumple):
     path = files + dumple
     sheets = main.sheet_generator(path)
-
     etalon_sheet = pd.read_excel('./etalon.xlsx', sheet_name=dumple)
-    assert all(sheets[dumple] == etalon_sheet)
+    assert_frame_equal(sheets[dumple], etalon_sheet)
 
-    etalon_sheet = pd.read_excel('./etalon.xlsx', sheet_name='Match ' + dumple)
-    assert all(sheets['Match ' + dumple] == etalon_sheet)
+    etalon_sheet = pd.read_excel('./etalon.xlsx', sheet_name='Match ' + dumple, converters={'AccountNumber': str})
+    sheets['Match ' + dumple] = sheets['Match ' + dumple].astype(data_types)
+    etalon_sheet = etalon_sheet.astype(data_types)
+    assert_frame_equal(sheets['Match ' + dumple], etalon_sheet)
